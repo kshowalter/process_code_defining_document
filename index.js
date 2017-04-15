65ad4d0201efa59afadc990d806af5ae5e0cdcda
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-04-14 21:45:39 -0400

    Added document output with code removed.

--- index.js
+++ index.js
@@ -19,12 +19,16 @@ var output_code_filename = args_array[1] || project_name+'.js' ;
 var project_path = path.resolve(process.argv[1], '../../../');
 var local_path = __dirname;
 
+// file names
+
 // file paths
-var input_md_path = path.join(project_path, input_md_filename);
-var output_code_path = path.join(project_path, output_code_filename);
-var output_pdf_path = path.join(project_path, project_name+'.pdf');
-var input_code_pre_path = path.join(project_path, output_code_filename+'.pre');
-var input_code_post_path = path.join(project_path, output_code_filename+'.post');
+var input_md_path =                 path.join(project_path, input_md_filename);
+var input_code_pre_path =           path.join(project_path, output_code_filename+'.pre');
+var input_code_post_path =          path.join(project_path, output_code_filename+'.post');
+var output_code_path =              path.join(project_path, output_code_filename);
+var output_source_pdf_path =        path.join(project_path, project_name+'.pdf');
+var output_standard_document_path = path.join(project_path, project_name+'_standard.md');
+var output_standard_pdf_path =      path.join(project_path, project_name+'_standard.pdf');
 
 var output_css_path = path.join(local_path, 'style.css');
 
@@ -32,11 +36,16 @@ var output_css_path = path.join(local_path, 'style.css');
 var input_string = fs.readFileSync(input_md_path, {encoding: 'utf8'});
 
 // extract and write code string
-var output_string = extract_code(input_string);
+var extract_code_output_obj = extract_code(input_string);
+var code_string = extract_code_output_obj.code_string;
 var input_string_pre = fs.readFileSync( input_code_pre_path, {encoding: 'utf8'}) || '';
 var input_string_post = fs.readFileSync( input_code_post_path, {encoding: 'utf8'}) || '';
-output_string = input_string_pre + output_string + input_string_post;
-fs.writeFileSync(output_code_path, output_string, {encoding: 'utf8'});
+code_string = input_string_pre + code_string + input_string_post;
+fs.writeFileSync(output_code_path, code_string, {encoding: 'utf8'});
+
+var standard_document_string = extract_code_output_obj.standard_document_string;
+fs.writeFileSync(output_standard_document_path, standard_document_string, {encoding: 'utf8'});
 
 // write pdf
-mk_pdf(input_md_path, output_pdf_path, output_css_path);
+mk_pdf(input_md_path, output_source_pdf_path, output_css_path);
+mk_pdf(output_standard_document_path, output_standard_pdf_path, output_css_path);
